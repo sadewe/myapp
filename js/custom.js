c8492810b4831e86c8f4437ba9bc9c8c5e128f63
s51757 <sarah91weber@web.de>
2015-10-29 18:38:46 +0100

    change

--- js/custom.js
+++ js/custom.js
@@ -5,5 +5,19 @@ $(document).ready(function(){
 function takePicture(){
 	$(".button--submit").bind("click", function(){
 		alert("Take a Picture");
+
+		navigator.camera.getPicture(onSuccess, onFail, {
+			quality: 10,
+			destinationType: Camera.destinationType.FILE_URI
+		});
 	});
+}
+
+function onSuccess(imageURI){
+	var image = $("myImage");
+	image.src = imageURI;
+}
+
+function onFail(message){
+	alert("Failed because: " + message);
 }
