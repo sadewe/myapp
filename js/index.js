d86def990d6fa6ffbab00b1490f52592668613a5
s51757 <sarah91weber@web.de>
2015-10-31 16:04:19 +0100

    change config, change function in index.js

--- js/index.js
+++ js/index.js
@@ -1,58 +1,25 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'takePicture'
-    // function, we must explicitly call 'app.takePicture(...);'
-    onDeviceReady: function() {
-        app.takePicture();
-    },
+$(document).ready(function(){
+    this.addEventListener('deviceready', onDeviceReady(), false);
+});
 
-    takePicture: function(){
+    function onDeviceReady() {
+        takePicture();
+    }
+    
+    function takePicture(){
         $(".button--submit").bind("click", function(){
-            alert("Take a Picture");
 
             navigator.camera.getPicture(onSuccess, onFail, {
                 quality: 10,
                 destinationType: Camera.destinationType.FILE_URI
             });
         });
-    },
+    }
 
-    onSuccess: function(imageURI){
-        var image = $(".myImage");
-        image.src = imageURI;
-    },
+    function onSuccess(imageURI){
 
-    onFail: function(message){
+    }
+
+    function onFail(message){
     alert("Failed because: " + message);
     }
-}
