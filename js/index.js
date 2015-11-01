4b663178a43edcf6fab0e6ba146e3e130b085bf7
s51757 <sarah91weber@web.de>
2015-11-01 14:35:36 +0100

    change js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,31 @@
-$(document).ready(function(){
+var app = {
 
-    this.addEventListener('deviceready', onDeviceReady, false);
+    takePicture: function(){
+        $(".button--submit").on("click", function(){
+            alert("Button was clicked");
+            navigator.camera.getPicture(onSuccess, onFail, {
+                quality: 10,
+                destinationType: Camera.destinationType.FILE_URI
+            });
+        });
+    }, 
+
+    onSuccess: function(imageURI){
+        alert("imageURI: " + imageURI);
+        var image = $(".myImage");
+        image.src = imageURI;
+    },
+
+    onFail: function(message){
+    alert("Failed because: " + message);
+    }
+
+};
+
+
+/*$(document).ready(function(){
+
+    this.addEventListener('deviceready', this.onDeviceReady, false);
     alert(onDeviceReady);
 });
 
@@ -9,15 +34,7 @@ $(document).ready(function(){
         takePicture();
     }
 
-    function takePicture(){
-        $(".button--submit").on("click", function(){
-            alert("Button was clicked");
-            navigator.camera.getPicture(onSuccess, onFail, {
-                quality: 10,
-                destinationType: Camera.destinationType.FILE_URI
-            });
-        });
-    }
+
 
     function onSuccess(imageURI){
         alert("imageURI: " + imageURI);
@@ -27,4 +44,4 @@ $(document).ready(function(){
 
     function onFail(message){
     alert("Failed because: " + message);
-    }
+    }*/
